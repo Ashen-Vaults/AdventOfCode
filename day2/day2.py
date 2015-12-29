0d907d3103ef6a82347b17aaede232dd20556393
Ashen-Vaults <alexhollander522@gmail.com>
2015-12-28 19:02:44 -0500

    updated day2

--- day2/day2.py
+++ day2/day2.py
@@ -1,23 +1,15 @@
-﻿global total_ribbons
-
-def readFile(file,readbyLine):
+﻿def readFile(file):
     total_surface = 0
     total_ribbons = 0
     
     with open(file, 'r') as f:
-        if readbyLine:
-            for line in f:
-                info = get_surface_area(line)
-                total_surface += info[0]
-                total_ribbons += info[1]
-            print("\n***TOTALS***")
-            print(total_surface)
-            print(total_ribbons)
-            
-        else:
-            first_line = f.readline();
-            print(first_line)
-            [x.strip() for x in first_line.__str__.split('x')]
+        for line in f:
+            info = get_surface_area(line)
+            total_surface += info[0]
+            total_ribbons += info[1]
+        print("\n***TOTALS***")
+        print(total_surface)
+        print(total_ribbons)
 
           
 def get_surface_area(line):
@@ -41,4 +33,4 @@ def get_ribbons(l,w,h):
      ribbon += dimensions 
      return ribbon           
         
-readFile('instructions.txt', True)
+readFile('instructions.txt')
---
+++ day3/day3.py
@@ -0,0 +1,35 @@
+def readFile(file,readbyLine):
+    total_surface = 0
+    total_ribbons = 0
+    
+    with open(file, 'r') as f:
+        if readbyLine:
+            for line in f:
+                info = get_surface_area(line)
+                total_surface += info[0]
+                total_ribbons += info[1]
+            print("\n***TOTALS***")
+            print(total_surface)
+            print(total_ribbons)
+            
+        else:
+            first_line = f.readline();
+            print(first_line)
+            [x.strip() for x in first_line.__str__.split('x')]
+
+readFile('instructions.txt', True)
+
+
+def get_surface_area(line):
+ 
+    l, w, h = map(int, line.split('x'))
+            
+    sides = []
+    sides.append(l*w)
+    sides.append(w*h)
+    sides.append(h*l)
+
+    smallest_side = min(int(x) for x in sides)
+    surface_area = ( (2 * sides[0] ) + (2 * sides[1]) + (2 * sides[2]) )
+    total = ((surface_area + smallest_side), get_ribbons(l,w,h))
+    return total
