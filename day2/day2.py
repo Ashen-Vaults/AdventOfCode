0f2afa9a1ed67586c339956998d32a84d796e99b
Ashen-Vaults <alexhollander522@gmail.com>
2015-12-28 23:00:31 -0500

    optimized day 2

--- day2/day2.py
+++ day2/day2.py
@@ -1,36 +1,31 @@
-﻿def readFile(file):
+﻿import cProfile
+
+def calculate_area_and_ribbons(file):
     total_surface = 0
     total_ribbons = 0
-    
     with open(file, 'r') as f:
         for line in f:
             info = get_surface_area(line)
             total_surface += info[0]
             total_ribbons += info[1]
-        print("\n***TOTALS***")
-        print(total_surface)
-        print(total_ribbons)
 
-          
 def get_surface_area(line):
- 
-    l, w, h = map(int, line.split('x'))
-            
+    l, w, h = map(int, line.split('x'))     
     sides = []
     sides.append(l*w)
     sides.append(w*h)
     sides.append(h*l)
-
     smallest_side = min(int(x) for x in sides)
-    surface_area = ( (2 * sides[0] ) + (2 * sides[1]) + (2 * sides[2]) )
-    total = ((surface_area + smallest_side), get_ribbons(l,w,h))
-    return total
+    surface_area = 2 * sides[0] + 2 * sides[1] + 2 * sides[2]
+    return surface_area + smallest_side, get_ribbons(l,w,h)
    
-
 def get_ribbons(l,w,h):
      dimensions = l * w * h
      ribbon = 2 * min(l+w, w+h, h+l)
      ribbon += dimensions 
-     return ribbon           
+     return ribbon  
+
+def main():
+    calculate_area_and_ribbons('instructions.txt')             
         
-readFile('instructions.txt')
+cProfile.run('main()')
